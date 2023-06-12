18a41c8b5baba79736a9148eafef899c7972e0ee
vladtymo <tymo.vlad@gmail.com>
2023-06-12 20:51:04 +0300

    Create Contact Book app using MVVM pattern.

---
+++ 08_mvvm_contacts/ViewModels/Contact.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+
+namespace _08_mvvm_contacts
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public bool IsBlocked { get; set; }
+
+        [DependsOn("Name", "Surname", "Phone", "IsBlocked")]
+        public string ShortInfo => $"{Name} {Surname} : {Phone} {(IsBlocked ? "X" : "")}";
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} : {Phone}";
+        }
+    }
+}
---
+++ 08_mvvm_contacts/ViewModels/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using PropertyChanged;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace _08_mvvm_contacts
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        private ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+
+        // -------- Binding Properties
+        public IEnumerable<Contact> Contacts => contacts;
+        public Contact SelectedContact { get; set; }
+
+        public ViewModel()
+        {
+            contacts.Add(new Contact() { Name = "Nastya", Surname = "Gogol", Age = 10, Phone = "+39090898857", IsBlocked = false });
+            contacts.Add(new Contact() { Name = "Viktor", Surname = "Fedor", Age = 20, Phone = "440-44-66", IsBlocked = true });
+            contacts.Add(new Contact() { Name = "Taras", Surname = "Tymo", Age = 18, Phone = "+39090565765", IsBlocked = false });
+        }
+
+        // -------- Presentation Logic
+        public void Dublicate()
+        {
+            if (SelectedContact != null)
+                // TODO: make deep copy
+                contacts.Add(SelectedContact); // shallow copy
+        }
+
+        public void Remove()
+        {
+            if (SelectedContact != null)
+                contacts.Remove(SelectedContact);
+        }
+    }
+}
